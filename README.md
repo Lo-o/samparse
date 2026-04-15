a13048ebf53051aa42f29d3b20eb307f1d9971f7
Luc Oostdijk <27276350+Lo-o@users.noreply.github.com>
2026-04-15 14:35:52 +0200

    include sam

--- README.md
+++ README.md
@@ -9,3 +9,25 @@ Afterwards, there is an issue with a circular reference. To fix:
 - remove the nonsensical line [System.Xml.Serialization.XmlIncludeAttribute(typeof(Be.Fgov.Ehealth.Samws.V2.Core.StandardResponseType))] above the filled StandardResponseType definition
 
 this is TODO to get fixed directly in the powershell script
+
+## Issues met namespace conflicts: 
+https://stackoverflow.com/questions/10532271/the-xml-element-named-name-from-namespace-references-distinct-types
+
+bijvoorbeeld conflict tussen deze 2: 
+
+`
+[System.Xml.Serialization.XmlElementAttribute("Title", Namespace="urn:be:fgov:ehealth:samws:v2:reimbursementlaw:submit")]
+public Be.Fgov.Ehealth.Samws.V2.Core.Text255Type Title { get; set; }
+`
+
+`
+[System.Xml.Serialization.XmlElementAttribute("Title", Namespace="urn:be:fgov:ehealth:samws:v2:reimbursementlaw:submit")]
+public Be.Fgov.Ehealth.Samws.V2.Core.TextType Title { get; set; }
+`
+
+
+## Aanpassingen gedaan: 
+Handmatige resolutio naar logische situatie (alle Text255Type veranderd naar TextType (meer permissive)
+
+voor deze: 
+- Text
---
+++ trial/Program.cs
@@ -0,0 +1,61 @@
+﻿// Deserialize from a file
+using Be.Fgov.Ehealth.Samws.V2.Company.Submit;
+using Be.Fgov.Ehealth.Samws.V2.Export;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Xml.Serialization;
+
+var overrides = new XmlAttributeOverrides();
+
+
+var serializer = new XmlSerializer(typeof(ExportCompaniesType), overrides);
+
+using var stream = File.OpenRead("./SAM/CPN-1775613602318.xml");
+var result = (ExportCompaniesType)serializer.Deserialize(stream);
+
+
+
+[XmlRootAttribute("SupportedIp", Namespace = "http://test.com/2010/test", IsNullable = false)]
+public partial class SupportedIp
+{
+    //[XmlElementAttribute(Namespace = "")]
+    [XmlElementAttribute(Namespace = "gabbagool")]
+    public string Name
+    {
+        get;
+        set;
+    } 
+}
+
+
+[GeneratedCodeAttribute("xsd", "2.0.50727.1432")]
+[SerializableAttribute()]
+[DebuggerStepThroughAttribute()]
+[DesignerCategoryAttribute("code")]
+[XmlTypeAttribute(Namespace = "http://test.com/2010/test")]
+[XmlRootAttribute("ObjectType", Namespace = "http://test.com/2010/test", IsNullable = false)]
+public partial class ObjectType
+{
+
+    /// <remarks/>
+    [XmlElementAttribute(ElementName = "", Namespace = "gabbagool")]
+    public LocalStrings Name
+    {
+        get;
+        set;
+    }
+
+    /// <remarks/>
+    [XmlArrayAttribute(ElementName = "Supportedip", Namespace = "")]
+    [XmlArrayItemAttribute(IsNullable = false, Namespace = "")]
+    public List<SupportedIp> Supportedip
+    {
+        get;
+        set;
+    }
+}
+
+public class LocalStrings
+{
+}
